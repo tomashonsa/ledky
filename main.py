02cdda59063de5025a43d13e157d9b879247b54d
Tomáš Honsa <Tomas.Honsa@student.souepl.cz>
2024-12-11 09:28:36 +0100

    m

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+from time import sleep
 import machine
-Pin1 = machine.Pin(0, machine.Pin.IN)
-pin2 = machine.Pin(1, machine.Pin.OUT)
+pin0 = machine.Pin(0, machine.Pin.IN)
+pin1 = machine.Pin(1, machine.Pin.OUT)
+pin2 = machine.Pin(2, machine.Pin.OUT)
+pin3 = machine.Pin(3, machine.Pin.OUT)
+pin4 = machine.Pin(4, machine.Pin.OUT)
+pin5 = machine.Pin(5, machine.Pin.OUT)
+
 while True:
-    status = Pin1.value()
-    if Pin1 == True:
-        Pin1.value(1)
+    status = pin0.value()
+    if status == True:
+        for x in range(5):
+            pin1.value(1)
+            pin3.value(1)
+            pin5.value(1)
+            sleep(1)
+            pin1.value(0)
+            pin3.value(0)
+            pin5.value(0)
+            pin2.value(1)
+            pin4.value(1)
+            sleep(1)
+            pin2.value(0)
+            pin4.value(0)
     else:
-        Pin1.value(0)
+        pin1.value(0)
